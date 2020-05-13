64c001726d0692c7166936f1c2f5cc142081e2da
Mahezz <atorresmaldonado21@gmail.com>
2020-05-13 12:09:03 +0200

    Revert "Actualizado"
    This reverts commit 6dc3f4071ab15a61de863b2db538e8ba1fd25695.

--- src/data/Main.java
+++ src/data/Main.java
@@ -17,7 +17,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Prueba de repositorio Jorge y Angel");
-        System.out.println("Actualización");
     }
     
 }
